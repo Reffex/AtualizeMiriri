ff3de0395e5bbe0c49f9f13775e6628ffd9b1031
Renan Filipe <renanf0705@gmail.com>
2025-06-20 10:01:49 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+    require_once 'includes/auto_check.php';
+    require_once 'includes/header.php';
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="./assets/css/styles.css">
+    <title>Atualize Miriri</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Bem Vindo <?php ?></h1>
+    </div>
+</body>
+</html>
+
+<?php
+    require_once 'includes/footer.php';
+?>
