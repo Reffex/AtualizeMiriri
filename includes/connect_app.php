f4025b01c0972ef38c7aa9619e7d5cd22bc648a3
Renan Filipe <renanf0705@gmail.com>
2025-06-25 15:35:32 -0300

    Update and rename connect.php to includes/connect_app.php

--- includes/connect_app.php
+++ includes/connect_app.php
@@ -1,13 +1,11 @@
 <?php
-
     $host = 'localhost';
     $user = 'root';
     $pass = '';
-    $bd = 'registros_clientes';
+    $bd = 'emprestimos';
 
     $mysqli = new mysqli ($host, $user, $pass, $bd);
     if ($mysqli->connect_errno) {
-       echo "Erro na conexão: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+       die ("Erro na conexão: " . $mysqli->connect_errno);
 }
-
 ?>
