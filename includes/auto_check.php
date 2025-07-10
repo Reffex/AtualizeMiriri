64c88a3dcfe572c2bed34e8297ea935d835bb264
Renan Filipe <renanf0705@gmail.com>
2025-07-10 11:58:39 -0300

    Update auto_check.php

--- includes/auto_check.php
+++ includes/auto_check.php
@@ -1,8 +1,7 @@
 <?php
-    session_start();
+session_start();
 
-    if(!isset($_SESSION['usuario_id'])) {
-        header("Location: ../login.php");
-        exit();
-    }
-?>
+if (!isset($_SESSION['usuario_id'])) {
+    header("Location: ../login.php");
+    exit();
+}
