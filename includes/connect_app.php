ca79086939c7e5fbe46fa76c2d6987d09efc3d41
Renan Filipe <renanf0705@gmail.com>
2025-07-10 11:59:39 -0300

    Update connect_app.php

--- includes/connect_app.php
+++ includes/connect_app.php
@@ -1,11 +1,10 @@
 <?php
-    $host = 'localhost';
-    $user = 'root';
-    $pass = '';
-    $bd = 'renanprojeto';
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+$bd = 'renanprojeto';
 
-    $mysqli = new mysqli ($host, $user, $pass, $bd);
-    if ($mysqli->connect_errno) {
-       die ("Erro na conexão: " . $mysqli->connect_errno);
+$mysqli = new mysqli($host, $user, $pass, $bd);
+if ($mysqli->connect_errno) {
+    die("Erro na conexão: " . $mysqli->connect_errno);
 }
-?>
