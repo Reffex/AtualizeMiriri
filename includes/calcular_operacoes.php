0334d285904810dfbe1c7ef4d0d5f5f209d7372a
Renan Filipe <renanf0705@gmail.com>
2025-07-17 07:12:18 -0300

    Update calcular_operacoes.php

--- includes/calcular_operacoes.php
+++ includes/calcular_operacoes.php
@@ -1,13 +1,13 @@
 <?php
 function calcular_operacao($mysqli, $operacao, $lancamentos)
 {
-    $valor_inicial = $operacao['valor_inicial'] ?? 0;
+    $valor_inicial = $operacao['valor_inicial'] ?? 0.0;
     $indexador = $operacao['indexador'];
     $data_inicio = new DateTime($operacao['data_criacao']);
     $data_fim = new DateTime($operacao['atualizar_ate']);
+    $juros_nominais = ($operacao['atualizar_juros_nominais'] ?? 0.0) / 100;
 
-    $periodicidade = $operacao['periodicidade'];
-    $dias_periodo = match ($periodicidade) {
+    $dias_periodo = match ($operacao['periodicidade']) {
         'Mensal' => 30,
         'Trimestral' => 90,
         'Semestral' => 180,
@@ -15,60 +15,52 @@ function calcular_operacao($mysqli, $operacao, $lancamentos)
         default => 30
     };
 
-    $periodos = new DatePeriod($data_inicio, new DateInterval("P{$dias_periodo}D"), $data_fim);
+    $periodos = new DatePeriod(
+        new DateTime($data_inicio->format('Y-m-01')),
+        new DateInterval("P1M"),
+        $data_fim
+    );
 
-    $correcao = 0;
+    $fator_total = 1.0;
+
     foreach ($periodos as $data) {
         $data_ref = $data->format('Y-m-01');
-        $stmt = $mysqli->prepare("SELECT valor FROM indices WHERE nome = ? AND data_referencia = ?");
-        $stmt->bind_param("ss", $indexador, $data_ref);
+
         $stmt = $mysqli->prepare("SELECT valor FROM indices WHERE nome = ? AND data_referencia = ?");
         $stmt->bind_param("ss", $indexador, $data_ref);
         $stmt->execute();
         $result = $stmt->get_result();
+        $indice = 0.0;
         if ($row = $result->fetch_assoc()) {
-            $correcao += $row['valor'];
+            $indice = $row['valor'] / 100; 
         }
         $stmt->close();
+
+        $fator_total *= (1 + $indice + $juros_nominais);
     }
-
-    $juros_total = 0;
-    $saldo_base = $valor_inicial;
-    $lancs = [];
-
-    $lancamentos->data_seek(0);
-    while ($l = $lancamentos->fetch_assoc()) {
-        $lancs[] = $l;
-        if ($l['tipo'] === 'credito') {
-            $saldo_base += $l['valor'];
-        } else {
-            $saldo_base -= $l['valor'];
-        }
-    }
-
-    foreach ($periodos as $data) {
-        $juros_nominais = $operacao['atualizar_juros_nominais'] ?? 0;
-        $juros_total += ($saldo_base * ($juros_nominais / 100));
-    }
-
 
     $saldo = $valor_inicial;
     $lancamentos->data_seek(0);
     while ($l = $lancamentos->fetch_assoc()) {
-        $saldo += ($l['tipo'] === 'credito') ? $l['valor'] : -$l['valor'];
+        $valor = $l['valor'];
+        $saldo += ($l['tipo'] === 'credito') ? $valor : -$valor;
     }
+
+    $valor_corrigido = $saldo * $fator_total;
 
     $multa = $operacao['valor_multa'] ?? 0;
     $honorarios = $operacao['valor_honorarios'] ?? 0;
 
-    $saldo_final = $saldo * (1 + ($correcao + $juros_total) / 100) + $multa + $honorarios;
+    $saldo_final = $valor_corrigido + $multa + $honorarios;
 
     return [
-        'correcao' => $correcao,
-        'juros' => $juros_total,
+        'fator_correcao' => $fator_total,
+        'valor_corrigido' => $valor_corrigido,
+        'juros_percentual' => $juros_nominais * 100,
         'multa' => $multa,
         'honorarios' => $honorarios,
         'saldo_atualizado' => $saldo_final,
         'movimentacao' => $saldo
     ];
 }
+?>
