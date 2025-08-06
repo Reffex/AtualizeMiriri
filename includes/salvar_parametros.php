b60c96f3310a9a9c97bea76d20b09d0ac3dc6226
Renan Filipe <renanf0705@gmail.com>
2025-08-06 15:54:10 -0300

    Update salvar_parametros.php

--- includes/salvar_parametros.php
+++ includes/salvar_parametros.php
@@ -8,15 +8,16 @@ $data = $_POST;
 
 try {
     // Converter valores para os tipos corretos
-    $divisor_juros = (int) $data['divisor_juros'];
-    $tipo_juros = $data['tipo_juros'];
-    $tipo_lancamento = $data['tipo_lancamento'];
-    $modo_operacao = $data['modo_operacao'];
-    $resolucao_2471 = (int) $data['resolucao_2471'];
-    $indexador_mensal = $data['indexador_mensal'];
-    $valores_negativos = (int) $data['valores_negativos'];
-    $corrigir_saldos = (int) $data['corrigir_saldos'];
-    $id = (int) $data['id'];
+    $divisor_juros = isset($data['divisor_juros']) ? (int) $data['divisor_juros'] : 360;
+    $tipo_juros = $data['tipo_juros'] ?? 'composto';
+    $tipo_lancamento = $data['tipo_lancamento'] ?? 'original';
+    $modo_operacao = $data['modo_operacao'] ?? 'outros';
+    $resolucao_2471 = isset($data['resolucao_2471']) ? (int) $data['resolucao_2471'] : 0;
+    $indexador_mensal = $data['indexador_mensal'] ?? 'mes_atual';
+    $valores_negativos = isset($data['valores_negativos']) ? (int) $data['valores_negativos'] : 0;
+    $corrigir_saldos = isset($data['corrigir_saldos']) ? (int) $data['corrigir_saldos'] : 0;
+    $id = isset($data['id']) ? (int) $data['id'] : 0;
+
 
     $stmt = $mysqli->prepare("UPDATE operacoes SET 
         divisor_juros = ?,
